30f1c8355995c411704cfd34e321b6e3e4e0408a
Simon Wong Chung <simon.wong.chung12@gmail.com>
2025-11-16 00:56:36 -0800

    Update

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="app-container px-6 py-15">
+      <div className="text-hero">
+        Simon designs authentic human experiences and brings clarity to complex problems. He strives to make our interactions with the world more thoughtful and beautiful.
+      </div>
+    </main>
+  );
+}
